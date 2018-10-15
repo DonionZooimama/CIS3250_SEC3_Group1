2b7d6cdae66863d83aeb545f0298555d7952764f
Daniel Ziorli <31424488+DonionZooimama@users.noreply.github.com>
2018-10-15 04:24:00 -0700

    Update playAgain.c

--- playAgain.c
+++ playAgain.c
@@ -2,9 +2,11 @@
 
 //Daniel
 void playAgain(char *playerInput , int *numberOfPlay, int *Height, char arr[100][100], char *arrSaveAction, int *savePlace,
-			   int *Width, int *Highscores, int *restorePlace, int *gameOut, char whichPlayer, int *scoreOne, int *scoreTwo, char *arrRestorePlace)
+	       int *Width, int *Highscores, int *restorePlace, int *gameOut, char whichPlayer, int *scoreOne, int *scoreTwo,
+	       char *arrRestorePlace)
 {
     printf("\n\t>>this move isn't valid \n\n >>do another move<< ");
     scanf("%s",playerInput);
-    player(playerInput , numberOfPlay, Height, arr, arrSaveAction, savePlace, Width, Highscores, restorePlace, gameOut, whichPlayer, scoreOne, scoreTwo, arrRestorePlace);
+    player(playerInput , numberOfPlay, Height, arr, arrSaveAction, savePlace, Width,
+	   Highscores, restorePlace, gameOut, whichPlayer, scoreOne, scoreTwo, arrRestorePlace);
 }
