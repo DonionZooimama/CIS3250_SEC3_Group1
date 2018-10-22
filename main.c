fc89893f264fbf08477033f95a45673e22bf7d3f
DerekBowman <43140374+DerekBowman@users.noreply.github.com>
2018-10-22 09:33:34 -0400

    Refactored main

--- main.c
+++ main.c
@@ -2,107 +2,106 @@
 
 int main()
 {
-	char arr[100][100];
-	char arrSaveAction[100] = {0}; 
-	char arrRestorePlace[100] = {0}; 
-	char playerInput[20];
+	char arr[ 100 ][ 100 ];
+	char arrSaveAction[ 100 ] = { 0 }; 
+	char arrRestorePlace[100] = { 0 }; 
+	char playerInput[ 20 ];
 	char whichPlayer = ' ';
-	char chose = 'C';
-    char option = ' ';
+	char userChoice = 'C';
+	char option = ' ';
 
-	int Width = 0;
-	int Height = 0;
-	int Highscores = 0;
+	int width = 0;
+	int height = 0;
+	int highscores = 0;
 	int numberOfPlay = 1;
 	int savePlace = 0;
 	int restorePlace = 0;
 	int scoreOne = 0;
 	int scoreTwo = 0;
 	int gameOut = 0;
-	int HighScoreList[50] = {0};
+	int highScoreList[ 50 ] = { 0 };
 	int listLength = 0;
-    int flag = 0;
+	int flag = 0;
 
-    while (chose =='C')
+    while ( userChoice =='C' )
     {
-        printf("\nIf You Want To Load The Saved Game >> Enter L <<Else Enter Any thing ");
-        fgets(&option, 3, stdin);
+        printf( "\nIf You Want To Load The Saved Game >> Enter L <<Else Enter Any thing " );
+        fgets( &option, 3, stdin );
 
-        if (option == 'L')
+        if ( option == 'L' )
         {
-            loadGame(&Width, &Height, &Highscores, &numberOfPlay, &scoreOne, &scoreTwo, &whichPlayer, &savePlace, &restorePlace, arr);
-			ReadXML(&Width, &Height, &Highscores);
-			arrInitialization(Height, Width, arr);
+            LoadGame( &width, &height, &highscores, &numberOfPlay, &scoreOne, &scoreTwo, &whichPlayer, &savePlace, &restorePlace, arr );
+	    ReadXML( &width, &height, &highscores );
+	    ArrInitialization( height, width, arr );
         }
         else
         {
-            printf("\n\t >> Human vs. Human << \n OR \n\t >> Human vs. Computer <<\n");
-            printf("\n>>Enter (H) if two player && (C) if one player\n");
-			fgets(&whichPlayer, 3, stdin);
+            printf( "\n\t >> Human vs. Human << \n OR \n\t >> Human vs. Computer <<\n" );
+            printf( "\n>>Enter (H) if two player && (C) if one player\n" );
+			fgets( &whichPlayer, 3, stdin );
 
-            while (whichPlayer != 'H' && whichPlayer != 'C')
+            while ( whichPlayer != 'H' && whichPlayer != 'C' )
             {
-                printf("\nsome thing error please enter again\n");
-                scanf("%s",&whichPlayer);
+                printf( "\nsome thing error please enter again\n" );
+                scanf( "%s",&whichPlayer );
             }
-            ReadXML(&Width, &Height, &Highscores);
-            arrInitialization(Height, Width, arr);
+            ReadXML(&width, &height, &highscores);
+            ArrInitialization(height, width, arr);
         }
 
-        while ( Width * Height >= numberOfPlay && gameOut != 1)
+        while ( width * height >= numberOfPlay && gameOut != 1 )
         {
-            PrintArrayValue(&Height, &Width, &scoreOne, &scoreTwo, arr);
+            PrintArrayValue( &height, &width, &scoreOne, &scoreTwo, arr );
 
-if (numberOfPlay % 2 != 0)
+    if ( numberOfPlay % 2 != 0 )
     {
-                printf("\n  |||||\n 0 . . 0\n0   ^   0\n0  \\_/  0\n 0     0\n  00000\n   888\n    8");
-                printf("\t player1<<Enter your play>> = ");
-                scanf("%s",playerInput);
-                player(playerInput , &numberOfPlay, &Height, arr, arrSaveAction, &savePlace, &Width, &Highscores, &restorePlace, &gameOut, whichPlayer, &scoreOne, &scoreTwo, arrRestorePlace);
-                numberOfPlay++;
-            }
+        printf( "\n  |||||\n 0 . . 0\n0   ^   0\n0  \\_/  0\n 0     0\n  00000\n   888\n    8" );
+        printf( "\t player1<<Enter your play>> = " );
+        scanf( "%s", playerInput );
+        Player( playerInput , &numberOfPlay, &height, arr, arrSaveAction, &savePlace, &width, &highscores, 
+	        &restorePlace, &gameOut, whichPlayer, &scoreOne, &scoreTwo, arrRestorePlace );
+        numberOfPlay++;
+    }
     else
-       {
-                if (whichPlayer == 'H')
-                {
-                    printf("\n ,---.\n/ o o \\ \n\\ \\_/ /\n `---`");
-                    printf("\tplayer2<<Enter your play>> = ");
-                    scanf("%s",playerInput);
-                    player(playerInput , &numberOfPlay, &Height, arr, arrSaveAction, &savePlace, &Width, &Highscores, &restorePlace, &gameOut, whichPlayer, &scoreOne, &scoreTwo, arrRestorePlace);
-                }
-                else
-                {
-                    computerPlayer(arr, &Height, &Width, &restorePlace, arrSaveAction, &savePlace, &scoreOne, &scoreTwo, &numberOfPlay, playerInput);
-                }
-
-                numberOfPlay++;
-            }
-        }
-
-        PrintArrayValue(&Height, &Width, &scoreOne, &scoreTwo, arr);
-        if (numberOfPlay-1 == Width*Height)
+    {
+        if ( whichPlayer == 'H' )
         {
-    if (scoreOne > scoreTwo)
-    {
-                printf("\n\n\t***player One is winner***");
-                printf(" \n\t **Hard luck player Two**\n");
-            }
-            else if (scoreOne < scoreTwo)
-            {
-                printf("\n\n\t***player Two is winner***");
-                printf(" \n\t **Hard luck player One**\n");
-            }
-            else
-    {
-                printf("\n\n\t***The two score is equivalent***\n\n");
-            }
-            flag = 1;
-			
-            HighScoresList(flag, HighScoreList, &listLength, &Highscores, scoreOne, scoreTwo);
+            printf( "\n ,---.\n/ o o \\ \n\\ \\_/ /\n `---`" );
+            printf( "\tplayer2<<Enter your play>> = " );
+            scanf( "%s", playerInput );
+            Player( playerInput , &numberOfPlay, &height, arr, arrSaveAction, &savePlace, &width, &highscores, 
+	            &restorePlace, &gameOut, whichPlayer, &scoreOne, &scoreTwo, arrRestorePlace );
         }
-        printf("\n\nYou Want To Continue Enter C else Enter any thing ");
-scanf("\n%s",&chose);
-        system("cls");
+        else
+        {
+            ComputerPlayer( arr, &height, &width, &restorePlace, arrSaveAction, &savePlace, &scoreOne, &scoreTwo, &numberOfPlay, playerInput );
+        }
+            numberOfPlay++;
+        }
+    }
+
+    PrintArrayValue( &height, &width, &scoreOne, &scoreTwo, arr );
+    if ( numberOfPlay-1 == width*height )
+    {
+        if ( scoreOne > scoreTwo )
+        {
+            printf( "\n\n\t***player One is winner***" );
+            printf( " \n\t **Hard luck player Two**\n" );
+        }
+        else if ( scoreOne < scoreTwo )
+        {
+            printf( "\n\n\t***player Two is winner***" );
+            printf( " \n\t **Hard luck player One**\n" );
+        }
+        else
+        {
+            printf( "\n\n\t***The two score is equivalent***\n\n" );
+        }
+            flag = 1;		
+            HighScoresList( flag, highScoreList, &listLength, &highscores, scoreOne, scoreTwo );
+        }
+        printf( "\n\nYou Want To Continue Enter C else Enter any thing " );
+        scanf( "\n%s", &userChoice );
     }
     return 0;
 }
